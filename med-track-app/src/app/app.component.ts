667fdf94fb96c1f6efeb8520892afa8e122b72b0
daramacmeanmain <dara.macmeanmain@outlook.ie>
2017-04-12 15:44:55 +0100

    fixed modal

--- med-track-app/src/app/app.component.ts
+++ med-track-app/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { AddMedPage } from '../pages/add-med/add-med';
 
 
 @Component({
