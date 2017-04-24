a4106f3bc2382bb3e632b431500490d254f2c1e9
daramacmeanmain <dara.macmeanmain@outlook.ie>
2017-04-25 00:39:26 +0100

    update and delete functions present but not yet fully working correctly

--- med-track-app/src/pages/user-profile/user-profile.ts
+++ med-track-app/src/pages/user-profile/user-profile.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Http } from '@angular/http';
+import { ActionSheetController, AlertController } from 'ionic-angular'
+import { Http, RequestOptions, Headers } from '@angular/http';
 
 /*
   Generated class for the UserProfile page.
@@ -16,8 +17,13 @@ export class UserProfilePage {
 
   public items: any = [];
   public uItems: any = [];
+  public medName: any;
+  public medDose: any;
+  public medFrq: any;
+  public userId: any;
+  private baseURI : String  = "http://51.141.24.34/"; 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {}
+  constructor(public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController, public navCtrl: NavController, public navParams: NavParams, public http: Http) {}
 
   ionViewWillEnter(){
       this.load();
@@ -40,6 +46,160 @@ export class UserProfilePage {
       {
          this.uItems = data;
       });
+   }
+
+   editMed(med, dose, frq, uid)
+  {
+    let body: String = "key=create&med=" + med + "&dose=" + dose + "&frq=" + frq + "&uid=" + uid,
+        type: String = "application/x-www-form-urlencoded; charset=UTF-8",
+        headers: any = new Headers({ 'Content-Type': type}),
+        options: any = new RequestOptions({ headers: headers }),
+        url 	 : any = this.baseURI + "medUpdate.php";
+
+    this.http.post(url, body, options).subscribe((data) => {
+      
+    });
+    
+  }
+
+  delMed(med, uid)
+  {
+    let body: String = "key=create&med=" + med + "&uid=" + uid,
+        type: String = "application/x-www-form-urlencoded; charset=UTF-8",
+        headers: any = new Headers({ 'Content-Type': type}),
+        options: any = new RequestOptions({ headers: headers }),
+        url 	 : any = this.baseURI + "medDelete.php";
+
+    this.http.post(url, body, options).subscribe((data) => {
+      
+    });
+    
+  }
+
+  setMed(item)
+   {
+      this.medName = item.med;
+      this.medDose = item.dose;
+      this.medFrq = item.frq;
+      this.userId = item.uid;
+   }
+
+   presentActionSheet(){
+     let actionSheet = this.actionSheetCtrl.create({
+       title: 'What would you like to do?',
+     buttons: [
+       {
+         text: 'Edit Prescription',
+         role: 'edit',
+         handler: () => {
+           console.log('edit clicked');
+           this.presentEdit();
+         }
+       },
+       {
+         text: 'Delete Prescription',
+         role: 'del',
+         handler: () => {
+           console.log('del clicked');
+           this.presentDelete();
+         }
+       },
+       {
+         text: 'Cancel',
+         role: 'cancel',
+         handler: () => {
+           console.log('cancel clicked');
+         }
+       }
+
+     ]
+    });
+    
+    actionSheet.present();
+   }
+
+   presentEdit() {
+      let editAlert = this.alertCtrl.create({
+      title: 'Edit',
+      inputs: [
+        {
+          name: 'mEdit',
+          placeholder: 'Medication'
+        },
+        {
+          name: 'dEdit',
+          placeholder: 'Dosage'
+        },
+        {
+          name: 'fEdit',
+          placeholder: 'Number of Times per Day'
+        },
+        {
+          name: 'uEdit',
+          placeholder: 'ID'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Confirm',
+          handler: data => {
+              let med : string = data.mEdit,
+                  dose : string = data.dEdit,
+                  frq : string = data.fEdit,
+                  uid : string = data.uEdit
+
+                  this.editMed(med, dose, frq, uid);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    editAlert.present();
+
+   }
+
+    presentDelete() {
+      let delAlert = this.alertCtrl.create({
+      title: 'Delete',
+      inputs: [
+        {
+          name: 'mDelete',
+          placeholder: 'Medication'
+        },
+        {
+          name: 'uDelete',
+          placeholder: 'ID'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Confirm',
+          handler: data => {
+           let med : string = data.mDelete,
+               uid : string = data.uDelete;
+
+               this.delMed(med, uid);
+          }
+        },
+      ]
+    });
+
+    delAlert.present();
+
    }
 
   ionViewDidLoad() {
